e8653ca92373f1c5fb1861a83120a48edfe8d8b6
Etless <askar2_00@hotmail.com>
2026-01-16 03:30:14 +0100

    Exercise 1

--- Exercise 1/__init__.py
+++ Exercise 1/__init__.py
@@ -1,3 +1,5 @@
+from sys import path_importer_cache
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
@@ -5,7 +7,7 @@ import numpy as np
 
 import a_star_algorithm as a_star
 
-WIDTH = 6
+"""WIDTH = 6
 MAP = [1, 1, 2, 2, 1, 1,
        1, 1, 1, 1, 1, 1,
        1, 1, 1, 1, 1, 1,
@@ -19,7 +21,7 @@ MAP = [1, 1, 2, 2, 1, 1,
 start = (1, 0)
 goal  = (4, 7)
 
-path = a_star.a_star_search(MAP, WIDTH, start, goal)
+path = a_star.a_star_search(MAP, WIDTH, start, goal, h=a_star.Diagonal)
 print(path)
 
 # Using code form Edgar Ramirez from stackoverflow
@@ -35,24 +37,79 @@ for j in range(len(MAP) // WIDTH):
 arr = np.array(MAP)
 arr = np.reshape(arr, (len(MAP) // WIDTH, WIDTH))
 
+# Adds the correct colors to the correct values
 cmap = mpl.colors.ListedColormap(["white", "green", "red"])
 bounds = [2, 10]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N, extend='both')
 
-plt.imshow(arr, cmap=cmap, norm=norm)
-plt.colorbar()
+fig, ax = plt.subplots(figsize=(6, 6), dpi=100)
+# Plot image of grid
+ax.imshow(arr, cmap=cmap, norm=norm, interpolation="none", aspect="equal")
 
-plt.axes().
-ax.set_xticks(np.arange(0, 10, 1))
-ax.set_yticks(np.arange(0, 10, 1))
+# Adds minor grid lines inbetween each cell
+ax.set_xticks(np.arange(-0.5, arr.shape[1], 1), minor=True)
+ax.set_yticks(np.arange(-0.5, arr.shape[0], 1), minor=True)
+ax.grid(which="minor")
+ax.tick_params(which="minor", bottom=False, left=False) # Hides the minor tick marks
+
+# Create chosen path
+path_np = np.array(path)
+
+plt.plot(path_np[:, 0], path_np[:, 1], color="blue", linewidth=3, marker="o", markersize=6)
+#plt.scatter(start[1], start[0], c="cyan", s=120, edgecolors="cyan", label="Start")
+#plt.scatter(goal[1], goal[0], c="yellow", s=120, edgecolors="yellow", label="Goal")
+
+#plt.colorbar()
+
+#plt.axes().set_xticks(np.arange(0, 10, 1))
+#plt.axes().set_yticks(np.arange(0, 10, 1))
 
 # Labels for major ticks
-ax.set_xticklabels(np.arange(1, 11, 1))
-ax.set_yticklabels(np.arange(1, 11, 1))
+#ax.set_xticklabels(np.arange(1, 11, 1))
+#ax.set_yticklabels(np.arange(1, 11, 1))
 
 #plt.axes().set_aspect('equal')
 #plt.xticks([])
 #plt.yticks([])
+plt.show()"""
+
+### ### GRID MAP ### ###
+GRID = np.array([
+       [1, 1, 2, 2, 1, 1,],
+       [1, 1, 1, 1, 1, 1,],
+       [1, 1, 1, 1, 1, 1,],
+       [1, 1,10,10, 2, 1,],
+       [1, 1, 1, 2, 2, 1,],
+       [1, 1, 1, 1, 1, 1,],
+       [1, 2,10,10, 1, 1,],
+       [1, 1, 1, 1, 1, 1,],
+       [1,10, 1, 1, 1, 1,]
+])
+
+start = (1, 0)
+goal  = (4, 7)
+
+path = a_star.test(GRID, start, goal, algorithm=a_star.Diagonal, iterations=1000000)
+
+# Adds the correct colors to the correct values
+cmap = mpl.colors.ListedColormap(["white", "green", "red"])
+bounds = [2, 10]
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N, extend='both')
+
+fig, ax = plt.subplots(figsize=(6, 6), dpi=100)
+# Plot image of grid
+ax.imshow(GRID, cmap=cmap, norm=norm, interpolation="none", aspect="equal")
+
+# Adds minor grid lines inbetween each cell
+ax.set_xticks(np.arange(-0.5, GRID.shape[1], 1), minor=True)
+ax.set_yticks(np.arange(-0.5, GRID.shape[0], 1), minor=True)
+ax.grid(which="minor")
+ax.tick_params(which="minor", bottom=False, left=False) # Hides the minor tick marks
+
+# Create chosen path
+path_np = np.array(path)
+plt.plot(path_np[:, 0], path_np[:, 1], color="blue", linewidth=3, marker="o", markersize=6)
+
 plt.show()
 
 
--- Exercise 1/a_star_algorithm.py
+++ Exercise 1/a_star_algorithm.py
@@ -1,9 +1,11 @@
-# A* algorithm
-import heapq
 # Used https://www.geeksforgeeks.org/dsa/a-search-algorithm/ for
 # reference/information gathering
-
+import heapq
 import math
+import warnings
+
+import numpy as np
+
 
 # Used to convert X, Y positions to array index
 def to_index(i, width)->int:
@@ -62,14 +64,14 @@ def a_star_search(grid, width, src, dest, h=Manhattan, iterations=0):
         raise Exception("Invalid grid size! Grid must be square!")
 
     # Checks if src & dest are valid
-    if 0 > src[0] > width or 0 > src[1] > height:
+    if 0 < src[0] >= width or 0 < src[1] >= height:
         raise Exception("Invalid source position! Source must be within grid!")
-    if 0 > dest[0] > width or 0 > dest[1] > height:
+    if 0 < dest[0] >= width or 0 < dest[1] >= height:
         raise Exception("Invalid destination position! Destination must be within grid!")
 
     # Make sure that source does not start in destination (not an error)
     if src == dest:
-        return
+        return src
 
     # The heap containing all "possible" moves. When the heap is writen to
     # heapq is used to quickly sort it depending on the cost
@@ -97,6 +99,10 @@ def a_star_search(grid, width, src, dest, h=Manhattan, iterations=0):
         cell = heapq.heappop(open_heap)
         pos = cell[1]
 
+        # Checks if cell is already closed
+        if closed_list[to_index(pos, width)]:
+            continue
+
         closed_list[to_index(pos, width)] = True  # Mark cell as visited
         cost_list[to_index(pos, width)] = (cell[0], cell[2]) # Mark cost of cell
 
@@ -105,7 +111,7 @@ def a_star_search(grid, width, src, dest, h=Manhattan, iterations=0):
             new_pos = (pos[0] + i[0], pos[1] + i[1])
 
             # Checks if cell is valid
-            if 0 > new_pos[0] > width or 0 > new_pos[1] > height:
+            if 0 < new_pos[0] >= width or 0 > new_pos[1] >= height:
                 continue
 
             # Checks if cell is already closed
@@ -127,8 +133,102 @@ def a_star_search(grid, width, src, dest, h=Manhattan, iterations=0):
                     path.append(cost_list[path[-1][0] + path[-1][1] * width][1])
                 path.reverse() # Reverse src -> dest
 
+                print(cost_list)
+
                 return path
 
+
+def test(grid, src, dest, algorithm=Manhattan, iterations=0):
+    height, width  = grid.shape[0], grid.shape[1]
+    iterations = width * height if iterations == 0 else iterations
+
+    # Makes sure that the grid map is valid
+    if grid.ndim != 2 or width == 0 or height == 0:
+        raise Exception("Invalid grid size! Grid must be a rectangle!")
+
+    # Flip (x, y) to (row, column) to match numpy
+    src = (src[1], src[0])
+    dest = (dest[1], dest[0])
+
+    # Checks if src & dest are valid
+    if 0 < src[1] >= width or 0 < src[0] >= height:
+        raise Exception("Invalid source position! Source must be within grid!")
+    if 0 < dest[1] >= width or 0 < dest[0] >= height:
+        raise Exception("Invalid destination position! Destination must be within grid!")
+
+    # Make sure that source does not start in destination (not an error)
+    if src == dest:
+        return src[1], src[0]  # (row, col) -> (x, y)
+
+    # The heap containing all "possible" moves. heapq is used when
+    # writing to quickly sort it depending on the cost
+    open_list = []
+    heapq.heappush(open_list, (0.0, src, src))  # Add src (pos, dest, parent)
+
+    # List of visited cells/positions. Used to remove overlapping
+    closed_list = np.zeros((height, width), dtype=bool)
+
+    # List of cost. Only shows for visited cells positions
+    cost_list = np.empty((height, width), dtype=float)
+
+    # List of initialized neighbor. Shows what cell activated it.
+    # Used to generate path at the end
+    path_list = np.empty((height, width, 2), dtype=int)
+
+    while True:
+        # Breaks the loop if iteration limit is exceeded
+        if iterations == 0:
+            warnings.warn("Ran out of iterations! Did not find a solution!")
+            return None
+        iterations -= 1
+
+        # Breaks the loop if heap is empty
+        if len(open_list) == 0:
+            warnings.warn("No solution found!")
+            return None
+
+        # Get the lowest cost cell
+        cost, pos, parent = heapq.heappop(open_list)
+        print(cost)
+        # Checks if cell is already closed
+        if closed_list[pos]:
+            continue
+
+        closed_list[pos] = True  # Mark cell as visited
+        cost_list[pos] = cost # Mark cost of cell
+        path_list[pos] = parent # Mark parent at cell
+
+        # Check all valid neighbors to the cell
+        for i in algorithm.directions:
+            new_pos = (pos[0] + i[0], pos[1] + i[1])
+
+            # Checks if cell is valid
+            if 0 < new_pos[1] >= width or 0 > new_pos[0] >= height:
+                continue
+
+            # Calculate the total cost of the cell
+            _cost = algorithm.distance(new_pos, dest) + grid[new_pos] + cost
+            heapq.heappush(open_list, (_cost, new_pos, pos))  # Add new positon to heap
+
+            # Check if cell is at destination
+            if new_pos == dest:
+                # Pretty print options
+                np.set_printoptions(
+                    precision=2,  # number of decimal places for floats
+                    suppress=True,  # suppress scientific notation
+                    linewidth=80,  # max characters per line
+                    threshold=100  # max elements before summarizing
+                )
+
+                print(cost_list)
+                # Make list with path from dest -> src (temp)
+                path = [dest, pos]
+                while not np.array_equal(path[-1], src):
+                    path.append((int(path_list[path[-1][0], path[-1][1], 0]), int(path_list[path[-1][0], path[-1][1], 1])))
+                #path.reverse()  # Reverse src -> dest
+                path = [(x, y) for y, x in reversed(path)] # Reverse src -> dest and (row, col) -> (x, y)
+
+                return path
 
 
 
